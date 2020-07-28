124fda770a17b179982cbffc4ae1e188801ab9ac
Harisa <harisaginting@gmail.com>
2020-07-28 19:29:52 +0700

    Updates

---
+++ precache-manifest.d7d9b2dfdc8652d5110fdfd82ed784d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe1e37631b1ff46920ee2d03118dd9a",
+    "url": "/curiculum-vitae/index.html"
+  },
+  {
+    "revision": "576057d350a6ef3ab79c",
+    "url": "/curiculum-vitae/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "d3f41e1cda7030c3ee11",
+    "url": "/curiculum-vitae/static/css/main.9eb5eafb.chunk.css"
+  },
+  {
+    "revision": "576057d350a6ef3ab79c",
+    "url": "/curiculum-vitae/static/js/2.5c29d68b.chunk.js"
+  },
+  {
+    "revision": "06ad0cfa56fa8a2c9ccc7b2fff0ec8e0",
+    "url": "/curiculum-vitae/static/js/2.5c29d68b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3f41e1cda7030c3ee11",
+    "url": "/curiculum-vitae/static/js/main.918000d6.chunk.js"
+  },
+  {
+    "revision": "0f2a2a2aaaedc086a4af",
+    "url": "/curiculum-vitae/static/js/runtime-main.fef9ec8e.js"
+  }
+]);
