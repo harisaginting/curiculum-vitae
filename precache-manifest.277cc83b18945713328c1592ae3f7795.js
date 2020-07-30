fca6e60ac1c9d190e3e25b5d2434ad5b3093f26c
Harisa <harisaginting@gmail.com>
2020-07-30 17:46:25 +0700

    Updates

--- precache-manifest.277cc83b18945713328c1592ae3f7795.js
+++ precache-manifest.277cc83b18945713328c1592ae3f7795.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4ee600a08ba1728a0739d7fd3c6a9d2d",
+    "revision": "ee5bd6b61e25fd3a26ee3cca143632ba",
     "url": "/curiculum-vitae/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/curiculum-vitae/static/css/2.ac1ab58b.chunk.css"
   },
   {
-    "revision": "de91307b97eff19de09c",
-    "url": "/curiculum-vitae/static/css/main.4ea730e4.chunk.css"
+    "revision": "55d90867a6e3190359e5",
+    "url": "/curiculum-vitae/static/css/main.94cffc5f.chunk.css"
   },
   {
     "revision": "e846e5812316befe7e23",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/curiculum-vitae/static/js/2.65d664f8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "de91307b97eff19de09c",
-    "url": "/curiculum-vitae/static/js/main.d5547789.chunk.js"
+    "revision": "55d90867a6e3190359e5",
+    "url": "/curiculum-vitae/static/js/main.67e828b5.chunk.js"
   },
   {
     "revision": "0f2a2a2aaaedc086a4af",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/curiculum-vitae/precache-manifest.ad5611fbb970f9e195906d9f874bd3b3.js"
+  "/curiculum-vitae/precache-manifest.277cc83b18945713328c1592ae3f7795.js"
 );
 
 self.addEventListener('message', (event) => {
